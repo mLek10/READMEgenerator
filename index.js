6973a15f6702a9cedc7082326302b5759b29f3b3
Emily Caton <caton.emily@yahoo.com>
2023-08-14 21:44:21 -0500

    add more prompts

--- index.js
+++ index.js
@@ -15,8 +15,43 @@ inquirer
       message: 'Enter a description:',
     },
     {
+        type: 'input',
+        name: 'table of contents',
+        message: 'Add table of contents:',
+      },
+    {
       type: 'input',
       name: 'installation',
       message: 'Enter installation instructions:',
     },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage examples:',
+      }, 
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Enter collaborators or third party assets:',
+      },
+      {
+        type: 'input',
+        name: 'license',
+        message: 'Choose a license:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter examples on how to run tests:',
+      },
+      {
+        type: 'input',
+        name: 'contribute',
+        message: 'Enter guidelines for contributing:',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: 'Enter email:',
+      },
   ])
